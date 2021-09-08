8d4eafed58f401f0469f75baa5010bd14606cb54
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-09-08 10:20:14 -0400

    first

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Task Tracker</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header', //Export name should be the same was the file name
+}
+</script>
+
+<style scoped> /*scoped - the css is only visible on this file template */
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
