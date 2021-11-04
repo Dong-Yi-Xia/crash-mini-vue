7d85eb1943630945366602d7e0e6e538cc0e4753
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-11-04 10:57:40 -0400

    data property

--- nuxt-notes/justVue.vue
+++ nuxt-notes/justVue.vue
@@ -1,3 +1,7 @@
+When using in-DOM templates (templates directly written in an HTML file),
+you should also avoid naming keys with uppercase characters,
+as browsers will coerce attribute names into lowercase:
+
 
 <template>
   <div>
@@ -11,18 +15,47 @@
     <a v-bind:href="url"> ... </a>
     <a v-on:click="doSomething"> ... </a>
 
+    <!-- Dynamic argument v-on:[eventName] will be equivalent to v-on:focus -->
+    <a v-on:[eventName]="doSomething"> ... </a>
+
     <!-- Add class in a object. Classname will be added if the value is TRUE -->
     <button
       type="button"
       class="reset-button search-icon"
       :class="{ 'search-trigger': isShowingSearchBar }"
     />
+
+    <!-- v-bind shorthand-->
+    <!-- full syntax -->
+    <a v-bind:href="url"> ... </a>
+    <!-- shorthand -->
+    <a :href="url"> ... </a>
+    <!-- shorthand with dynamic argument -->
+    <a :[key]="url"> ... </a>
+
+    <!-- v-on shorthand -->
+    <!-- full syntax -->
+    <a v-on:click="doSomething"> ... </a>
+    <!-- shorthand -->
+    <a @click="doSomething"> ... </a>
+    <!-- shorthand with dynamic argument -->
+    <a @[event]="doSomething"> ... </a>
+
   </div>
 </template>
 
 <script>
 export default {
+  // Vue uses a $ prefix when exposing its own built-in APIs via the component instance.
+  // It also reserves the prefix _ for internal properties.
 
+  // data function. these instance properties are only added when the instance is first created,
+  // use null, undefined or placeholder value. until desired value is available.
+  data() {
+    return {
+      count: 4
+    }
+  }
 }
 </script>
 
