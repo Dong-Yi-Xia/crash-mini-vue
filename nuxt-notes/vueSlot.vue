f331f0dbd0451d25a37516d167fbf0097a16ed1a
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-11-08 16:47:01 -0500

    slots

---
+++ nuxt-notes/vueSlot.vue
@@ -0,0 +1,25 @@
+<template>
+  <todo-button> Add todo Something </todo-button>
+</template>
+
+
+// child componenet, todo-button componenet
+<template>
+  <button class="btn-primary">
+    <slot>Default/Fallback </slot>
+  </button>
+</template>
+
+
+<template>
+  <!-- rendered HTML -->
+  <button class="btn-primary">
+    Add todo Something
+  </button>
+
+  <!-- if nothing was passed inside the component in the parent, fallback takes its place-->
+  <!-- Slot is flexible in reusing code -->
+   <button class="btn-primary">
+    Default/Fallback
+  </button>
+</template>
